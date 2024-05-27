09d63289ba78487b2e984615ce20066dbf7762d1
Nguyễn Khắc Hậu <22520410@gm.uit.edu.vn>
2024-05-27 10:33:35 +0700

    init lab5

---
+++ Lab5/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tbFrom = new System.Windows.Forms.TextBox();
+            this.tbTo = new System.Windows.Forms.TextBox();
+            this.tbPwd = new System.Windows.Forms.TextBox();
+            this.tbSubject = new System.Windows.Forms.TextBox();
+            this.rtbBody = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(23, 35);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(139, 59);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // tbFrom
+            // 
+            this.tbFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.tbFrom.Location = new System.Drawing.Point(258, 35);
+            this.tbFrom.Name = "tbFrom";
+            this.tbFrom.Size = new System.Drawing.Size(257, 27);
+            this.tbFrom.TabIndex = 1;
+            // 
+            // tbTo
+            // 
+            this.tbTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.tbTo.Location = new System.Drawing.Point(258, 83);
+            this.tbTo.Name = "tbTo";
+            this.tbTo.Size = new System.Drawing.Size(257, 27);
+            this.tbTo.TabIndex = 2;
+            // 
+            // tbPwd
+            // 
+            this.tbPwd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.tbPwd.Location = new System.Drawing.Point(712, 35);
+            this.tbPwd.Name = "tbPwd";
+            this.tbPwd.Size = new System.Drawing.Size(257, 27);
+            this.tbPwd.TabIndex = 3;
+            // 
+            // tbSubject
+            // 
+            this.tbSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.tbSubject.Location = new System.Drawing.Point(201, 144);
+            this.tbSubject.Name = "tbSubject";
+            this.tbSubject.Size = new System.Drawing.Size(768, 27);
+            this.tbSubject.TabIndex = 4;
+            // 
+            // rtbBody
+            // 
+            this.rtbBody.Location = new System.Drawing.Point(201, 208);
+            this.rtbBody.Name = "rtbBody";
+            this.rtbBody.Size = new System.Drawing.Size(768, 343);
+            this.rtbBody.TabIndex = 5;
+            this.rtbBody.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(636, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Password:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "From:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(211, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "To:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(119, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Subject:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(120, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Body:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1115, 576);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbBody);
+            this.Controls.Add(this.tbSubject);
+            this.Controls.Add(this.tbPwd);
+            this.Controls.Add(this.tbTo);
+            this.Controls.Add(this.tbFrom);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tbFrom;
+        private System.Windows.Forms.TextBox tbTo;
+        private System.Windows.Forms.TextBox tbPwd;
+        private System.Windows.Forms.TextBox tbSubject;
+        private System.Windows.Forms.RichTextBox rtbBody;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Lab5/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            using (SmtpClient smtpClient = new SmtpClient("127.0.0.1"))
+            {
+                if (tbFrom.Text.Length == 0 || tbTo.Text.Length == 0 || tbPwd.Text.Length == 0 || tbSubject.Text.Length == 0 || rtbBody.Text.Length == 0)
+                {
+                    MessageBox.Show("Vui lòng nhập đầy đủ thông tin!");
+                    return;
+                }
+                if (!tbFrom.Text.Contains("@"))
+                {
+                    MessageBox.Show("Thông tin nhập trường From phải là email");
+                    return;
+                }
+                if (!tbFrom.Text.EndsWith("@nhomHH.nt106"))
+                {
+                    MessageBox.Show("Email gửi phải thuộc domain @nhomHH.nt106");
+                    return;
+                }
+                if (!tbTo.Text.Contains("@"))
+                {
+                    MessageBox.Show("Thông tin nhập trường To phải là email");
+                    return;
+                }
+                if (!tbTo.Text.EndsWith("@nhomHH.nt106"))
+                {
+                    MessageBox.Show("Email nhận phải thuộc domain @nhomHH.nt106");
+                    return;
+                }
+                string mailfrom = tbFrom.Text.ToString().Trim();
+                string mailto = tbTo.Text.ToString().Trim();
+                string password = tbPwd.Text.ToString().Trim();
+                var basicCredential = new NetworkCredential(mailfrom, password);
+                try
+                {
+                    using (MailMessage message = new MailMessage())
+                    {
+                        MailAddress fromAddress = new MailAddress(mailfrom);
+                        smtpClient.UseDefaultCredentials = false;
+                        smtpClient.Credentials = basicCredential;
+                        message.From = fromAddress;
+                        message.Subject = tbSubject.Text.ToString().Trim();
+                        // Set IsBodyHtml to true means you can send HTML email.
+                        message.IsBodyHtml = true;
+                        message.Body = rtbBody.Text.ToString();
+                        message.To.Add(mailto);
+                        tbFrom.Clear();
+                        tbTo.Clear();
+                        tbPwd.Clear();
+                        tbSubject.Clear();
+                        rtbBody.Clear();
+                        try
+                        {
+                            smtpClient.Send(message);
+                            MessageBox.Show("Mail gửi thành công");
+                        }
+                        catch (Exception ex)
+                        {
+                            //MessageBox.Show(ex.Message.ToString());
+                            if (ex.Message.ToString().Contains("Authentication required"))
+                            {
+                                MessageBox.Show("Sai mật khẩu");
+                            }
+                        }
+                    }
+                } catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+                
+            }
+        }
+    }
+}
