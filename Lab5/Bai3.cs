476f669c901abc863901b918ca0bbf37923e93bb
Nguyễn Khắc Hậu <22520410@gm.uit.edu.vn>
2024-06-03 23:20:48 +0700

    add bai3

---
+++ Lab5/Bai3.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5
+{
+    public partial class Bai3 : Form
+    {
+        public Bai3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAttach_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    tbAttach.Text = ofd.FileName;
+                }
+            }  
+        }
+
+        private void CleanUp()
+        {
+            tbFrom.Text = "";
+            tbTo.Text = "";
+            tbSubject.Text = "";
+            tbPassword.Text = "";
+            rtbBody.Text = "";
+            tbAttach.Text = "";
+        }
+
+        private void SendMail(string subject, string body, string mailto, string mailfrom, string password)
+        {
+            using (SmtpClient smtpClient = new SmtpClient("smtp.gmail.com"))
+            {
+                smtpClient.Port = 587;
+                smtpClient.EnableSsl = true;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(mailfrom, password);
+
+                using (MailMessage message = new MailMessage())
+                {
+                    MailAddress fromAddress = new MailAddress(mailfrom);
+                    message.From = fromAddress;
+                    message.Subject = subject;
+                    message.IsBodyHtml = true;
+                    message.To.Add(mailto);
+                    message.Body = body;
+
+                    if (tbAttach.Text != "")
+                    {
+                        Attachment attachment = new Attachment(tbAttach.Text);
+                        message.Attachments.Add(attachment);
+                    }
+
+                    try
+                    {
+                        CleanUp();
+                        smtpClient.Send(message);
+                        MessageBox.Show("Send successfully!");
+                        
+
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show(e.ToString());
+                        CleanUp();
+                    }
+                }
+            }
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string mailfrom = tbFrom.Text.Trim();
+            string mailto = tbTo.Text.Trim();
+            string password = tbPassword.Text.Trim();
+            string subject = tbSubject.Text.Trim();
+            string body = rtbBody.Text.Trim();
+
+            try
+            {
+                SendMail(subject, body, mailto, mailfrom, password);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+    }
+}
